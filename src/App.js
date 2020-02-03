4c0107a44fe865dc683868bc5b0c2f2f16afe1da
Dang Van Cuong <cuongdang1997@gmail.com>
2020-02-03 17:03:33 +0700

    Build UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload, test auto deploy.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">      
+        <h2>Convert virtual currency</h2>        
       </header>
+      <div className="container">
+        <input type="text" placeholder="Enter number"/>
+        <select>
+          <option value="volvo">BTC</option>
+          <option value="saab">ETH</option>
+          <option value="mercedes">XRP</option>
+        </select>
+        <span>To</span>
+        <select>
+          <option value="volvo">BTC</option>
+          <option value="saab">ETH</option>
+          <option value="mercedes">XRP</option>
+        </select>
+      </div>
     </div>
   );
 }
